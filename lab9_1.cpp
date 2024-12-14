db5cbaed467fee550bf9bff819f9c4b620a6c931
naechutimon <chutimonnaenae@gmail.com>
2024-12-14 23:18:32 +0700

    chunelab9/1

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,33 +1,47 @@
 #include<iostream>
-#include<iomanip> //For using setw(), setprecision(), ...
+#include<iomanip> 
 using namespace std;
 
-int main(){	
+int main(){
+    
+    double initial,rate,amount;
 	cout << "Enter initial loan: ";
+	cin>>initial;
 	cout << "Enter interest rate per year (%): ";
+	cin>>rate;
 	cout << "Enter amount you can pay per year: ";
+	cin>>amount;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+    int year = 0;
+    double balance=initial;
+    
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
-	cout << setw(13) << left << "Total";
+	cout << setw(13) << left << "Total"; 
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+    cout << fixed << setprecision(2); 
+    
+    while (balance>0) {
+        double interest = balance*(rate/100);
+        double total = balance+interest;
+        double payment = (total>amount)? amount : total;
+        double newbalance = total-payment;
+        year++;
+        
+        cout << setw(13) << left << year; 
+	    cout << setw(13) << left << balance;
+	    cout << setw(13) << left << interest;
+	    cout << setw(13) << left << total;
+	    cout << setw(13) << left << payment;
+	    cout << setw(13) << left << newbalance;
+	    cout << "\n";
+	
+	    balance = newbalance ;
+    }
 	
 	return 0;
 }
